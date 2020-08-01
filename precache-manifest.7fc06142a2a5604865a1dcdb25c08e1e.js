fda3d3ee39d2136f52813d447c5b48519226ba55
cn14 <chethan.171co113@nitk.edu.in>
2020-08-01 07:31:13 +0530

    Updates

---
+++ precache-manifest.7fc06142a2a5604865a1dcdb25c08e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e304e51bac5e61ff05fb53edfca94d",
+    "url": "/React-Tinder-clone/index.html"
+  },
+  {
+    "revision": "80d235020fb3ee886026",
+    "url": "/React-Tinder-clone/static/css/main.bfe6f845.chunk.css"
+  },
+  {
+    "revision": "495f967d47f1134bed65",
+    "url": "/React-Tinder-clone/static/js/2.bc771839.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/React-Tinder-clone/static/js/2.bc771839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d235020fb3ee886026",
+    "url": "/React-Tinder-clone/static/js/main.222aa666.chunk.js"
+  },
+  {
+    "revision": "1ed22073db72cd6aacb3",
+    "url": "/React-Tinder-clone/static/js/runtime-main.14e1800d.js"
+  }
+]);
